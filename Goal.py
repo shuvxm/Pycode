9b910e85af0bf098301b425d30ca2dcbb8d7ed47
shuvxm <kumarshubham5559@gmail.com>
2024-12-22 18:55:33 +0530

    up

---
+++ Goal.py
@@ -0,0 +1,9 @@
+# => Searching, Sorting program
+
+# => Sliding Window  technique questions like this to get the clue of this technique
+#  -> finding subarray with given sum
+#  -> find maximum sum of a subarray of size k
+#  -> smallest window in a string containing all characters of other string
+#  -> Length of the longest substring without repeating characters
+#  -> Count distinct elements in every window of size k
+
